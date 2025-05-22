2eb0c2f4aff8e98fcd6d85ee68679667cee014e5
yuliia volkova <yuliia_volkova@icloud.com>
2025-05-22 18:40:44 +0200

    started main info + styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
