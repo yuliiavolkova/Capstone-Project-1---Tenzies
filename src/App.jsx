78a986b1272d28bc79c8ff5d780961e2f3498e0b
yuliia volkova <yuliia_volkova@icloud.com>
2025-05-23 19:10:00 +0200

    die.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+import Die from "./Die.jsx";
+
 export default function App() {
-  return <main>Type</main>;
+  return (
+    <main>
+      <div className="dice-container">
+        <Die value={1} />
+        <Die value={2} />
+        <Die value={3} />
+        <Die value={4} />
+        <Die value={5} />
+        <Die value={6} />
+        <Die value={7} />
+        <Die value={8} />
+        <Die value={9} />
+        <Die value={10} />
+      </div>
+    </main>
+  );
 }
